89b31d871995e4506074985a08a5a98a471832d0
Obsidian Maximus <obsidianmaximus@gmail.com>
2022-10-01 19:18:20 +0530

    Create Firstquestion.py

---
+++ Answers/Firstquestion.py
@@ -0,0 +1,23 @@
+'''1)Print the pattern-
+a)*
+  **
+  ***
+  **** 
+b)****
+  ***
+  **
+  *
+'''
+
+
+
+#Answers - 
+
+print("a)*")
+print("  **")
+print("  ***")
+print("  ****")
+print("b)****")
+print("  ***")
+print("  **")
+print("  *")
