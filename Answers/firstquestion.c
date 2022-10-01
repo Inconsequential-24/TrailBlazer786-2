c3d6f4e9fdba243208db937370dddf14232472c9
Obsidian Maximus <obsidianmaximus@gmail.com>
2022-10-02 00:07:35 +0530

    Add files via upload
    Already did first question using py, but just did this to test my own self as i dont know much about C (did logic building from py)

---
+++ Answers/firstquestion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+
+{
+	clrscr();
+	printf("Solution to Question 1: \n");
+	printf("\n");
+	
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("****\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*");
+	
+}
+
+/// Had solved it already using py, but now used C just to test my own self as am completely new to it.
